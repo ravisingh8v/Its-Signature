1c509a67109df91a1e98423b05dd7945e5fd01e5
ravisingh8v <ravisingh8v@gmail.com>
2024-03-15 17:37:49 +0530

    fix bug

--- src/components/ImagePreviewer.tsx
+++ src/components/ImagePreviewer.tsx
@@ -13,16 +13,16 @@ export default function ImagePreviewer({
     <>
       <img src={src} className="h-[300px] w-full rounded border bg-gray-100" />
       <div className="grid sm:grid-cols-2 justify-between gap-x-2">
-        <div className="sm:col-span-1 ">
+        <div className="sm:col-span-1 col-span-full">
           <a
             href={src}
-            className="block h-full w-full border border-green-200"
+            className="block h-full w-full"
             download={true}
           >
             <Button type="primary">Download</Button>
           </a>
         </div>
-        <div className="sm:col-span-1">
+        <div className="sm:col-span-1 col-span-full">
           <Button type="secondary" onClick={() => clearSignature()}>
             Delete
           </Button>
